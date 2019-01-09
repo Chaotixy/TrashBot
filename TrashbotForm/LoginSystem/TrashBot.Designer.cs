afb7bb6e5b614d77829063603d460e9589195180
LAMB407\Ajala samuel <imisiajalaxingmx@yahoo.com>
2019-01-09 13:31:25 +0100

    Homepage

---
+++ TrashbotForm/LoginSystem/TrashBot.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace TrashbotForm
+{
+    partial class TrashBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrashBot));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.search_button = new System.Windows.Forms.Button();
+            this.longitude = new System.Windows.Forms.Label();
+            this.longitude_txt = new System.Windows.Forms.TextBox();
+            this.latitude_txt = new System.Windows.Forms.TextBox();
+            this.latitude = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(26, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 71);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(191)))), ((int)(((byte)(170)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(804, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "LOG OUT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(191)))), ((int)(((byte)(170)))));
+            this.label1.Location = new System.Drawing.Point(119, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 59);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Trash Bot";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(49, 121);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(762, 403);
+            this.map.TabIndex = 13;
+            this.map.Zoom = 0D;
+            // 
+            // search_button
+            // 
+            this.search_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.search_button.Location = new System.Drawing.Point(917, 501);
+            this.search_button.Name = "search_button";
+            this.search_button.Size = new System.Drawing.Size(75, 23);
+            this.search_button.TabIndex = 18;
+            this.search_button.Text = "search";
+            this.search_button.UseVisualStyleBackColor = true;
+            // 
+            // longitude
+            // 
+            this.longitude.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.longitude.AutoSize = true;
+            this.longitude.Location = new System.Drawing.Point(832, 458);
+            this.longitude.Name = "longitude";
+            this.longitude.Size = new System.Drawing.Size(50, 13);
+            this.longitude.TabIndex = 17;
+            this.longitude.Text = "longitude";
+            // 
+            // longitude_txt
+            // 
+            this.longitude_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.longitude_txt.Location = new System.Drawing.Point(917, 452);
+            this.longitude_txt.Name = "longitude_txt";
+            this.longitude_txt.Size = new System.Drawing.Size(100, 20);
+            this.longitude_txt.TabIndex = 16;
+            // 
+            // latitude_txt
+            // 
+            this.latitude_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.latitude_txt.Location = new System.Drawing.Point(917, 413);
+            this.latitude_txt.Name = "latitude_txt";
+            this.latitude_txt.Size = new System.Drawing.Size(100, 20);
+            this.latitude_txt.TabIndex = 15;
+            // 
+            // latitude
+            // 
+            this.latitude.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.latitude.AutoSize = true;
+            this.latitude.Location = new System.Drawing.Point(841, 420);
+            this.latitude.Name = "latitude";
+            this.latitude.Size = new System.Drawing.Size(41, 13);
+            this.latitude.TabIndex = 14;
+            this.latitude.Text = "latitude";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(944, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 31);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Copyright TB";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(191)))), ((int)(((byte)(170)))));
+            this.panel1.Location = new System.Drawing.Point(-1, 552);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1059, 39);
+            this.panel1.TabIndex = 12;
+            // 
+            // TrashBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(1056, 590);
+            this.Controls.Add(this.search_button);
+            this.Controls.Add(this.longitude);
+            this.Controls.Add(this.longitude_txt);
+            this.Controls.Add(this.latitude_txt);
+            this.Controls.Add(this.latitude);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TrashBot";
+            this.ShowInTaskbar = false;
+            this.Text = "Trash Bot";
+            this.Load += new System.EventHandler(this.TrashBot_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Button search_button;
+        private System.Windows.Forms.Label longitude;
+        private System.Windows.Forms.TextBox longitude_txt;
+        private System.Windows.Forms.TextBox latitude_txt;
+        private System.Windows.Forms.Label latitude;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
